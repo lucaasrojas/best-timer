464cbadc8106cdb7cd4303bcacc9ddc38b40e997
Lucas Rojas <lucas.rojas@live.com.ar>
2021-05-22 14:45:53 -0300

    Update styles and logic to edit time

--- src/App.js
+++ src/App.js
@@ -2,12 +2,19 @@ import React, { useEffect, useRef, useCallback } from 'react';
 import {Route, Switch, HashRouter} from "react-router-dom"
 import {default as Timer} from './Components/Timer'
 import {default as Welcome} from './Components/Welcome'
+// TODO: 
+// - Pagina de ayuda con instrucciones (el inicio? timer/)
+// - Cambio de colores
+// - Cambio de direccion de progreso
+// - Notificacion
+
 function App() {
 
 
   return (
     <HashRouter>
       <Switch>
+        <Route exact path="/" component={Timer} />
         <Route exact path="/:time" component={Timer} />
       </Switch>
     </HashRouter>
--- src/Components/Timer.jsx
+++ src/Components/Timer.jsx
@@ -1,18 +1,50 @@
 import React, { useEffect, useRef, useCallback } from 'react';
 
 const Timer = (props) => {
-    console.log("TIMER", props)
+    //console.log("TIMER", props)
     const [time, setTime] = React.useState({ min: 0, sec: 0 })
     const [initialTime, setInitialTime] = React.useState({ min: 0, sec: 0 })
     const [running, setRunning] = React.useState(false)
     const [progressPercentage, setProgressPercentage] = React.useState(0)
+    const [editSelected, setEditSelected] = React.useState("min")
     let interval
 
     const KEY_FUNCTIONS = {
         "Space": () => {
-            console.log('SPACE', running, time);
-
             setRunning(!!!running)
+        },
+        "KeyR": () => {
+            setRunning(false)
+            setTime({ min: 0, sec: 0 })
+            setProgressPercentage(0)
+        },
+        "ArrowUp": () => {
+            if (editSelected === "sec" && (time.sec + 5) < 59) {
+                setInitialTime(prevTime => ({ ...prevTime, sec: prevTime.sec + 5 }))
+                setTime(prevTime => ({ ...prevTime, sec: prevTime.sec + 5 }))
+            } else if (editSelected === "min" && (time.min + 5) < 59) {
+                setInitialTime(prevTime => ({ ...prevTime, min: prevTime.min + 5 }))
+                setTime(prevTime => ({ ...prevTime, min: prevTime.min + 5 }))
+            }
+        },
+        "ArrowDown": () => {
+            if (editSelected === "sec" && (time.sec - 5) > 0) {
+                setInitialTime(prevTime => ({ ...prevTime, sec: prevTime.sec + 5 }))
+                setTime(prevTime => ({ ...prevTime, sec: prevTime.sec - 5 }))
+            } else if (editSelected === "min" && (time.min - 5) > 0) {
+                setInitialTime(prevTime => ({ ...prevTime, min: prevTime.min + 5 }))
+                setTime(prevTime => ({ ...prevTime, min: prevTime.min - 5 }))
+            }
+        },
+        "ArrowLeft": () => {
+            if (editSelected === "sec") {
+                setEditSelected("min")
+            }
+        },
+        "ArrowRight": () => {
+            if (editSelected === "min") {
+                setEditSelected("sec")
+            }
         }
     }
     const DEFAULT_KEY = () => null
@@ -45,22 +77,31 @@ const Timer = (props) => {
         );
     }
     useEventListener("keydown", (key) => {
-        KEY_FUNCTIONS[key.code]() || DEFAULT_KEY()
-    })
-    React.useEffect(() => {
-        const timeFromPath = getTimeFromPath()
-        if (timeFromPath.min > 0 || timeFromPath.sec > 0) {
-            setTime(timeFromPath)
-            setInitialTime(timeFromPath)
+        console.log("KEY", key)
+        if(KEY_FUNCTIONS[key.code]) {
+            KEY_FUNCTIONS[key.code]() 
         }
-    }, [])
-
-    React.useEffect(() => {
-        console.log('TIME CHANGED', time);
+    })
+    useEventListener("touchend", (key) => {
+        KEY_FUNCTIONS["Space"]() || DEFAULT_KEY()
+    })
+    const calculatePercentage = () => {
         const { min, sec } = time
         const timeInSec = (min * 60) + sec
         const initialTimeInSec = (initialTime.min * 60) + initialTime.sec
         setProgressPercentage((timeInSec * 100) / initialTimeInSec)
+    }
+    React.useEffect(() => {
+        const timeFromPath = getTimeFromPath()
+        if (timeFromPath.min > 0 || timeFromPath.sec > 0) {
+            setInitialTime(timeFromPath)
+            setTime(timeFromPath)
+        }
+    }, [])
+
+    React.useEffect(() => {
+        //console.log('TIME CHANGED', time);
+        calculatePercentage()
     }, [time])
 
     React.useEffect(() => {
@@ -88,13 +129,14 @@ const Timer = (props) => {
 
     const getTimeFromPath = () => {
         let response = { min: 0, sec: 0 }
-        let pathname = props.match.params.time;
-        console.log('pathnname', window.location.pathname);
+        if (props.match.params && props.match.params.time) {
+            let pathname = props.match.params.time;
 
-        if (pathname.trim()) {
-            pathname = pathname.replace("minutes", '.').replace("seconds", "").replace("minute", '.').replace("second", "").replace("min", '.').replace("sec", "")
-            const [min, sec] = pathname.split(".")
-            response = { min: parseInt(min), sec: parseInt(sec) }
+            if (pathname.trim()) {
+                pathname = pathname.replace("minutes", '.').replace("seconds", "").replace("minute", '.').replace("second", "").replace("min", '.').replace("sec", "")
+                const [min, sec] = pathname.split(".")
+                response = { min: parseInt(min), sec: parseInt(sec) }
+            }
         }
 
         return response
@@ -106,11 +148,28 @@ const Timer = (props) => {
                 display: "flex",
                 alignItems: "center",
                 justifyContent: "center",
-                position: "relative"
+                position: "relative",
+                backgroundColor: "#150e56"
             }}
         >
+            <div id="help-text"
+                style={{
+                    position: "absolute",
+                    top: 0,
+                    left: 0,
+                    paddingLeft: 10,
+                    fontSize: "1.5vw",
+                    zIndex: 1
+                }}
+            >
+                <p>Touch screen: Start/Pause Timer</p>
+                <p>Space: Start/Pause Timer</p>
+                <p>R: Reset Timer</p>
+                <p>Left/Right Arrow: Select edit</p>
+                <p>Up/Down Arrow: Change value</p>
+            </div>
             <div style={{
-                background: "red",
+                background: "rgb(111, 74, 161)",
                 height: "inherit",
                 transition: "width .2s ease-in",
                 position: "absolute",
@@ -119,7 +178,7 @@ const Timer = (props) => {
             }}></div>
             <div
                 style={{
-                    fontSize: "10rem",
+                    fontSize: "10vw",
                     position: "absolute",
                     top: 0, left: 0, right: 0, bottom: 0,
                     marginLeft: "auto",
@@ -127,7 +186,23 @@ const Timer = (props) => {
                     inset: "unset"
                 }}
             >
-                {time.min.toString().length > 1 ? time.min : `0${time.min}`}:{time.sec.toString().length > 1 ? time.sec : `0${time.sec}`}
+                <span
+                    style={{
+                        color: editSelected === "min" && !running ? "#686868" : "#d0d0d0"
+                    }}
+                >
+                    {time.min.toString().length > 1 ? time.min : `0${time.min}`}
+                </span>
+                <span style={{color: "#faf3f3"}}>
+                    :
+                </span>
+                <span
+                    style={{
+                        color: editSelected === "sec" && !running  ? "#686868" : "#d0d0d0"
+                    }}>
+
+                    {time.sec.toString().length > 1 ? time.sec : `0${time.sec}`}
+                </span>
             </div>
         </div>
     )
